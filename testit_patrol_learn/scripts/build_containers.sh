229d93fdf2a9f9de0a8adfb25e5d581ac281ae7c
ingmar <ingmar@liibert.ee>
2020-05-24 16:36:28 +0300

    Update

--- testit_patrol_learn/scripts/build_containers.sh
+++ testit_patrol_learn/scripts/build_containers.sh
@@ -2,7 +2,7 @@
 
 echo "Building testit_patrol_sut container..."
 cd $(rospack find testit_patrol)/docker/sut
-docker build --no-cache -t testitros/testit_patrol_sut:spinning .
+docker build --no-cache -t ingmarliibert/testit_patrol_sut:latest .
 echo "Building testit_patrol_testit container..."
 cd $(rospack find testit_patrol)/docker/testit
-docker build --no-cache -t testitros/testit_patrol_testit:learn .
+docker build --no-cache -t ingmarliibert/testit_patrol_testit:latest .
